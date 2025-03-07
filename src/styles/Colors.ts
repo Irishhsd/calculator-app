15392e4ef43d10caeb4ccab5818c7671111465c7
Irishhsd <saucedodelgadoi@gmail.com>
2025-03-07 09:02:14 -0600

    colors changed

--- src/styles/Colors.ts
+++ src/styles/Colors.ts
@@ -1,11 +1,11 @@
 export const myColors = {
-    light: '#F1F2F3',
-    dark: '#17171C',
-    blue: '#4B5EFC',
-    btnGray: '#4E505F',
-    btnDark: '#2E2F38',
-    gray: '#747477',
-    black: '#000000',
-    white: '#FFFFFF', 
-    result: '#46D5B2',
-}
+    light: "#F1F2F3",
+    dark: "#17171C",
+    blue: "#d98f21", //naranja
+    btnGray: "#8f8d89",
+    btnDark: "#2E2F38",
+    gray: "#747477",
+    black: "#52514f",
+    white: "#FFFFFF",
+    result: "#5b5e5d", //gris
+  };
